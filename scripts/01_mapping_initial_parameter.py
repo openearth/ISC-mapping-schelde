d59e93f4d241f43453853d4b4fed72c818b40c9f
nathaliedees <64775981+nathaliedees@users.noreply.github.com>
2026-04-08 16:48:50 +0200

    eerste aanpassingen naar de WADAR test
    nog niet perfect, maar getest met de eerste WADAR uitdraai

--- scripts/01_mapping_initial_parameter.py
+++ scripts/01_mapping_initial_parameter.py
@@ -5,13 +5,14 @@ from pathlib import Path
 
 p=Path(os.getcwd())
 donar = pd.read_csv(Path.joinpath(p.parent, 'voorbeeld/isc2024/isc2024.csv'), sep =';')
+wadar = pd.read_csv(Path.joinpath(p.parent, 'voorbeeld/WADAR/ISC2024/ISC2024.csv'), sep = ';', low_memory = False)
 gevraagd_format = pd.read_excel(Path.joinpath(p.parent, 'voorbeeld/ISC-CIE WGM_Tranfert des données RHME 2024_Sept 2025.xlsx'), sheet_name=None)
 aangeleverd_2024 =  pd.read_excel(Path.joinpath(p.parent, 'voorbeeld/ISC-CIE WGM_Oct 2025_NL.xlsx'), sheet_name=None)
 aquo = pd.read_csv(Path.joinpath(p.parent, 'AQUO/Parameter.csv'), sep =';')
 hoedanigheid = pd.read_csv(Path.joinpath(p.parent, 'AQUO/Hoedanigheid.csv'), sep=';')
 # %%
 # preprocessing, unpacking multiple excel sheets and selecting what is needed for the analysis
-donarcols = donar.columns
+wadarcols = wadar.columns
 sheets = gevraagd_format.keys()
 sheetsa = aangeleverd_2024.keys()
 tables = [k for k in  sheetsa] # list of desired cols
@@ -32,7 +33,7 @@ meetdata_mapping['Geanalyseerde fractie'] = meetdata_mapping['Geanalyseerde frac
 # %% parameter mapping
 #gewoon paroms mappen naar de ISC tabel die gegeven is, geeft maar 6 matches, dit is dus niet wat je wil gebruiken
 # PARCODE gebruiken om te mappen naar AQUO
-#eerst dan mapping op CAS nummer naar ISC daarna DONAR naar de CAS + ICS tabel
+#eerst dan mapping op CAS nummer naar ISC daarna wadar naar de CAS + ICS tabel
 aquo = aquo[aquo['Status'].isin(['Geldig'])]
 aquo = aquo.rename(columns={'Omschrijving': 'AQUO_Omschrijving',
                             'Codes': 'AQUO_Codes'})
@@ -42,9 +43,9 @@ aquocols = ['CASnummer',
             ]
 casnummers = aquo[aquocols]
 
-paroms = donar[['PAROMS', 'PAR','HDH']].drop_duplicates()
-paroms = paroms.rename(columns = {'PAROMS': 'DONAR_Parameter',
-                                  'PAR': 'DONAR_PARCode'})
+# paroms = wadar[['PAROMS', 'PAR','HDH']].drop_duplicates()
+paroms = wadar[['grootheid_code','parameter_code', 'hoedanigheid_code']].drop_duplicates()
+paroms = paroms.rename(columns = {'parameter_code': 'wadar_PARCode'})
 
 # %% # MAP ISC table to CAS nummers
 # casnummers = casnummers[casnummers['CASnummer'] != 'NVT'] #otherwise not one to one mapping
@@ -77,36 +78,37 @@ print('total parameters =', len(parameterx),
       '\n NO match ISC with CAS=', len(leftjoin),
       '\n SUM', len(innerjoin)+ len(leftjoin))
 
-# Join DONAR met complete ISC mapping met CAS nummers en kijk wat er wel en niet gemapt kan worden van DONAR naar ISC. 
+# Join wadar met complete ISC mapping met CAS nummers en kijk wat er wel en niet gemapt kan worden van wadar naar ISC. 
 # Hebben we alle variabelen die nodig zijn?
-donar_isc_cas = innerjoin.merge(paroms, left_on='AQUO_Codes', right_on='DONAR_PARCode', how='inner') #, validate='one_to_one'
-nomatch_donar_isc_cas = (innerjoin.merge(paroms, left_on='AQUO_Codes', right_on='DONAR_PARCode', how = 'left', indicator=True) #Alle ISC die niet met CASnummers kunnen worden gemapt
+wadar_isc_cas = innerjoin.merge(paroms, left_on='AQUO_Codes', right_on='wadar_PARCode', how='inner') #, validate='one_to_one'
+nomatch_wadar_isc_cas = (innerjoin.merge(paroms, left_on='AQUO_Codes', right_on='wadar_PARCode', how = 'left', indicator=True) #Alle ISC die niet met CASnummers kunnen worden gemapt
                         .query('_merge == "left_only"')
                         .drop('_merge', axis=1))
 
-print('total can be mapped from DONAR to ISC =', len(donar_isc_cas), 
-      '\n Cannot find match for ISC parameters with DONAR', len(nomatch_donar_isc_cas),
-      '\n SUM', len(donar_isc_cas)+ len(nomatch_donar_isc_cas))
+print('total can be mapped from wadar to ISC =', len(wadar_isc_cas), 
+      '\n Cannot find match for ISC parameters with wadar', len(nomatch_wadar_isc_cas),
+      '\n SUM', len(wadar_isc_cas)+ len(nomatch_wadar_isc_cas))
 
-nomatch =nomatch_donar_isc_cas.merge(leftjoin, on = 'Unieke identificatie gemeten parameter', how='outer', suffixes=("", "_r"))
+nomatch =nomatch_wadar_isc_cas.merge(leftjoin, on = 'Unieke identificatie gemeten parameter', how='outer', suffixes=("", "_r"))
 
 # Identify overlapping non-key columns that exist on both sides
-overlaps = set(nomatch_donar_isc_cas.columns).intersection(leftjoin.columns) - set(['Unieke identificatie gemeten parameter'])
+overlaps = set(nomatch_wadar_isc_cas.columns).intersection(leftjoin.columns) - set(['Unieke identificatie gemeten parameter'])
 
 #%%
 # Coalesce: prefer left, then right
 for col in overlaps:
     nomatch[col] = nomatch[col].combine_first(nomatch.pop(f"{col}_r"))
 
-donar_isc_cas.to_csv(Path.joinpath(p.parent,'mappings/donar-isc-cas.csv'), index=False)
-nomatch.to_csv(Path.joinpath(p.parent,'mappings/nomatch-donar-isc-cas.csv'), index=False)
+wadar_isc_cas.to_csv(Path.joinpath(p.parent,'mappings/wadar-isc-cas.csv'), index=False)
+nomatch.to_csv(Path.joinpath(p.parent,'mappings/nomatch-wadar-isc-cas.csv'), index=False)
 
 with pd.ExcelWriter(Path.joinpath(p.parent,'mappings/parameter_e_h_g.xlsx')) as writer: 
-    donar_isc_cas.to_excel(writer, sheet_name='donar_isc_cas', index=False)
-    nomatch.to_excel(writer, sheet_name='nomatch_donar_isc_cas', index=False) 
+    wadar_isc_cas.to_excel(writer, sheet_name='wadar_isc_cas', index=False)
+    nomatch.to_excel(writer, sheet_name='nomatch_wadar_isc_cas', index=False) 
 
 # %% location mapping
-lsloc=donar['LOCOMS'].drop_duplicates().to_frame()
+# MET wadar een handmatige mapping is nodig aangezien de nieuwe terminologie te veel verschilt 
+lsloc=wadar['locatie_code'].drop_duplicates().to_frame()
 locs= locations[['Identitication unique de la station' , 'Localité']]
 locs=locs.groupby(['Identitication unique de la station' , 'Localité']).count().reset_index()
 locs=locs[locs['Identitication unique de la station'].str.startswith("NL")]
@@ -116,4 +118,4 @@ all_locs.to_csv(Path.joinpath(p.parent,'mappings/locations.csv'), index=False)
 all_locs.to_excel(Path.joinpath(p.parent,'mappings/locations-raw.xlsx'), index=False)
 
 # %%
-# mapping van donar HDH uniques met hoedanigheid codes uniques
+# mapping van wadar HDH uniques met hoedanigheid codes uniques
